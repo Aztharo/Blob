4930cdd03873ea36d8dc1df114e118c014fc6d59
Aztharo <artur.zhyhaylo@gmail.com>
2019-08-08 20:19:26 +0200

    Initial commit

---
+++ Blob/Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player; 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0, 0, -20);
+    }
+}
---
+++ Blob/Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 10f;
+    public float jumpheight = 500f;
+    public bool jumpReady;
+    void Start()
+    {
+
+    }
+
+    public void OnCollisionEnter2D()
+    {      
+            jumpReady = true;
+
+    }
+
+    void FixedUpdate()
+    {
+        
+        if (Input.GetKeyDown("w") && jumpReady)
+        {
+            rb.AddForce(new Vector2(0, jumpheight));
+            jumpReady = false;
+
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position = transform.position + new Vector3(-speed, 0) * Time.deltaTime;
+
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position = transform.position + new Vector3(speed, 0) * Time.deltaTime;
+
+        }
+
+    }
+}
