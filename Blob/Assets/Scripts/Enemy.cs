8d0b8d824bac2a5cef6e9d8695c0068c03b6a286
Aztharo <artur.zhyhaylo@gmail.com>
2019-08-09 10:17:09 +0200

    09.08.12019, 10:00
    Changelog:
    - PlayerMovement updated
    - Enemies were added
    - Sprites!

---
+++ Blob/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public float retDis;
+
+    private Transform player;
+    public GameObject projectile;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) > retDis) {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }   else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+        if(timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        } else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
---
+++ Blob/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 500f;
+    public float jumpheight = 600f;
+    public bool jumpReady;
+    public float torquespeed = 150f;
+    float yForce;
+    float xForce;
+    float xTorque;
+
+    void Start()
+    {
+
+    }
+
+
+    public void OnCollisionEnter2D()
+    {
+        jumpReady = true;
+
+    }
+
+    void FixedUpdate()
+    {
+
+        float xInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown("w") && jumpReady)
+        {
+            rb.AddForce(new Vector2(0, jumpheight));
+            jumpReady = false;
+        }
+        xForce = xInput * speed * Time.deltaTime;
+        xTorque = xInput * torquespeed * Time.deltaTime;
+
+        Vector2 force = new Vector2(xForce, 0);
+        rb.AddForce(force);
+        rb.AddTorque(-xTorque);
+            
+    }
+}
