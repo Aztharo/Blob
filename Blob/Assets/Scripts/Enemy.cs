b7d7bc3c157d90385b44586d052f34fe576abaf3
Aztharo <artur.zhyhaylo@gmail.com>
2019-08-09 17:21:02 +0200

    09.08.12019 17:20

--- Blob/Assets/Scripts/Enemy.cs
+++ Blob/Assets/Scripts/Enemy.cs
@@ -4,9 +4,13 @@ public class Enemy : MonoBehaviour
 {
     public float speed;
     public float retDis;
+    public float stopDis;
+    public float bulletSpeed;
 
     private Transform player;
     public GameObject projectile;
+    public GameObject shootpoint;
+    public GameObject middle;
 
     private float timeBtwShots;
     public float startTimeBtwShots;
@@ -20,20 +24,41 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, player.position) > retDis) {
+        Vector3 difference = player.transform.position - shootpoint.transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        middle.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+
+        if (Vector2.Distance(transform.position, player.position) > stopDis) {
             transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-        }   else
+        }
+        else if(Vector2.Distance(transform.position, player.position) > retDis && Vector2.Distance(transform.position, player.position) <stopDis) 
+        {
+
+        }
+        else if(Vector2.Distance(transform.position, player.position) < retDis)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
         }
 
         if(timeBtwShots <= 0)
         {
-            Instantiate(projectile, transform.position, Quaternion.identity);
+            float distance = difference.magnitude;
+            Vector2 direction = difference / distance;
+            direction.Normalize();
+            fireBullet(direction, rotationZ);
+            
             timeBtwShots = startTimeBtwShots;
         } else
         {
             timeBtwShots -= Time.deltaTime;
         }
+
+    }
+    void fireBullet(Vector2 direction, float rotationZ)
+    {
+        GameObject b = Instantiate(projectile) as GameObject;
+        b.transform.position = shootpoint.transform.position;
+        b.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+        b.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
     }
 }
